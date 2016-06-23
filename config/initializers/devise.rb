57fa5d635e98042cccf9ac5e9d30ed48a66b477f
Misigno <efournier92@gmail.com>
2016-06-22 23:34:05 -0400

    fix devise error in production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,5 @@
 Devise.setup do |config|
 
-  config.mailer_sender = 'no-reply@' + Rails.application.secrets.domain_name
-
   require 'devise/orm/active_record'
 
   config.case_insensitive_keys = [:email]
